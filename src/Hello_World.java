03b919f145e5a046c61a633e704c23defada2ba8
charu <charudhingra0808@gmail.com>
2025-08-31 19:20:36 +0530

    Hello World with intellij usage as a beginner.

--- src/Hello_World.java
+++ src/Hello_World.java
@@ -1,4 +1,6 @@
 //Printing hello world in java
+//Used intellij and just downloaded fron internet , then do edit configurations > add sdk and for the main class
+//just add the one class having main method and we are good to go
 
 public class Hello_World {
     public static void main(String[] args) {
